722a5f5d923ee8038c03b2d21c4c97e8209096b5
nikosdano <danopoulosnikos@gmail.com>
2014-02-14 15:09:14 +0200

    Web Seminar

---
+++ upload.php
@@ -0,0 +1,16 @@
+<?php
+	$destination = "style/";
+		if( !empty( $_FILES ) ) {
+			$destination .= $_FILES[ "file" ][ "name" ];
+			$filename = $_FILES[ "file" ][ "tmp_name" ];
+			move_uploaded_file($filename, $destination);
+			}	        
+	echo "Uploaded file: " . $_FILES[ "file" ][ "name" ]. "</br>";
+        echo "Type: " . $_FILES[ "file" ][ "type" ]. "</br>";
+        echo "Size: " . $_FILES[ "file" ][ "size" ]. "kb"."</br>";
+	echo "Destination: " . $destination . "</br>";
+	$open = opendir("style") ;
+		while(false != ($file = readdir( $open ))) {
+			echo $file . "</br>";
+	}
+?> 
