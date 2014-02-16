95f3e9c4a91b649a695c40bb80504a453acc3a7e
nikosdano <danopoulosnikos@gmail.com>
2014-02-16 19:07:55 +0200

    Changes

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html>
-<body>
+	<body>
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
-<label for="file">Filename:</label>
-<input type="file" name="file" id="file"><br>
-<input type="submit" name="submit" value="Submit">
-</form>
+		<form action="upload.php" method="post" enctype="multipart/form-data">
+			<label for="file">Filename:</label>
+				<input type="file" name="file" id="file"><br>
+				<input type="submit" name="submit" value="Submit">
+		</form>
 
-</body>
+	</body>
 </html> 
--- upload.php
+++ upload.php
@@ -1,17 +1,17 @@
 <?php
 	$destination = "style";
-		if( !empty( $_FILES ) ) {
+		if (!empty( $_FILES ) ) {
 			$destination .= $_FILES[ "file" ][ "name" ];
 			$filename = $_FILES[ "file" ][ "tmp_name" ];
 			move_uploaded_file($filename, $destination);
-			}	        
+		}	        
 	echo "Uploaded file: " . $_FILES[ "file" ][ "name" ]. "</br>";
         echo "Type: " . $_FILES[ "file" ][ "type" ]. "</br>";
         echo "Size: " . $_FILES[ "file" ][ "size" ]. "kb"."</br>";
 	echo "Destination: " . $destination . "</br>";
 	$open = opendir("style") ;
-		while(false != ($file = readdir( $open ))) {
-			if( $file != "." && $file != "..") {
+		while (false != ($file = readdir ( $open ) ) ) {
+			if ($file != "." && $file != "..") {
 				echo "Files uploaded: " . $file . "</br>";
 			}
 		}
